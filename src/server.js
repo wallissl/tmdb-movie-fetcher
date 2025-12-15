4d9da278e26f4fe579267abb2997e7dc28ad179b
Wallis <wallis.sl@outlook.com>
2025-12-15 10:03:09 -0300

    Organização do código

--- src/server.js
+++ src/server.js
@@ -1,21 +1,9 @@
-/* import express from "express";
-import cors from "cors";
-import router from "./routes/routes.js"; */
-
 import app from "./app.js";
 import dotenv from "dotenv";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
-
-/* const app = express();
-
-app.use(cors());
-app.use(express.json());
-
-//rotas
-app.use("/api", router); */
 
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
